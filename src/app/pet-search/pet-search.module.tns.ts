723378104fb90c2cf15485292ce7f4a8dc7f90da
sebawita <sebawita@gmail.com>
2017-10-03 13:19:51 +0100

    Adding UI for NativeScript

--- src/app/pet-search/pet-search.module.tns.ts
+++ src/app/pet-search/pet-search.module.tns.ts
@@ -1,9 +1,10 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { SHARED_MODULES, COMPONENT_DECLARATIONS } from './pet-search.common';
-
+import { NativeScriptUIDataFormModule } from 'nativescript-telerik-ui-pro/dataform/angular';
 @NgModule({
   imports: [
-    ...SHARED_MODULES
+    ...SHARED_MODULES,
+    NativeScriptUIDataFormModule
   ],
   declarations: [
     ...COMPONENT_DECLARATIONS
